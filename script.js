4c65770b8cb3da0fd9a1ae27f73ca68d4154bf22
Keith Bailey <bailey.keith.allen@gmail.com>
2020-07-22 17:56:04 -0400

    jQuery to create adaptive responses

--- script.js
+++ script.js
@@ -12,29 +12,29 @@ function goAdv() {
     let pet = document.getElementById('pet').value;
         
         if (loc == 1 && pet == 1){
-            html += ("Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze")
-            console.log(html);
+             document.getElementById("responseText").innerHTML = "Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze";
+             document.getElementById("response").src = "pics/SweetDee.jpg";
         } else if (loc == 1 && pet == 2){
-            html +=  ("As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.")
+            document.getElementById("responseText").innerHTML =  ("As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.")
         } else if (loc == 1 && pet == 3){
-            html += ("Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.")
+            document.getElementById("responseText").innerHTML = ("Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.")
         } else if (loc ==1 && pet == 4){
-            html += ("Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/")  
+            document.getElementById("responseText").innerHTML = ("Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/")  
         } else if (loc == 2 && pet == 1){
-            html += ("Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!")  
+            document.getElementById("responseText").innerHTML = ("Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!")  
         } else if (loc == 2 && pet == 2){
-            html += ("Dennis smiles and stays by your side throughout the walk :)")  
+            document.getElementById("responseText").innerHTML = ("Dennis smiles and stays by your side throughout the walk :)")  
         } else if (loc == 2 && pet == 3){
-            html += ("Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.")  
+            document.getElementById("responseText").innerHTML = ("Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.")  
         } else if (loc == 2 && pet == 4){
-            html += ("Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.")  
+            document.getElementById("responseText").innerHTML = ("Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.")  
         } else if (loc == 3 && pet == 1){
-            html += ("Dee is scared. She hears the bath water, tucks her tail ebtween her legs, and hurries to the comfort of her kennel. Dennis begins to bark in her defense.")  
+            document.getElementById("responseText").innerHTML = ("Dee is scared. She hears the bath water, tucks her tail ebtween her legs, and hurries to the comfort of her kennel. Dennis begins to bark in her defense.")  
         } else if (loc == 3 && pet == 2){
-            html += ("Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)")  
+            document.getElementById("responseText").innerHTML = ("Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)")  
         } else if (loc == 3 && pet == 3){
-            html += ("Mac enjoys the feel of the water and is a trooper through the process.")  
+            document.getElementById("responseText").innerHTML = ("Mac enjoys the feel of the water and is a trooper through the process.")  
         } else if (loc == 3 && pet == 4){
-            html += ("Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear.")  
+            document.getElementById("responseText").innerHTML = ("Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear.")  
         } 
 }
