d272e3862a3bdaec73751fbc24fdb6ad979b1392
Keith Bailey <bailey.keith.allen@gmail.com>
2020-07-24 19:52:28 -0400

    added response images

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-let html = " ";
-
-
 //function will take the activity and pet chosen by the user from the dropdown menus. 
 //From there it assesses the selections and responds with the chosen pet's reaction to the activity.
 
@@ -13,28 +8,39 @@ function goAdv() {
         
         if (loc == 1 && pet == 1){
              document.getElementById("responseText").innerHTML = "Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze";
-             document.getElementById("response").src = "pics/SweetDee.jpg";
+             document.getElementById("response").src = "pics/DeeRide.jpg";
         } else if (loc == 1 && pet == 2){
             document.getElementById("responseText").innerHTML =  ("As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.")
+            document.getElementById("response").src = "pics/Dennis.jpg";
         } else if (loc == 1 && pet == 3){
             document.getElementById("responseText").innerHTML = ("Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.")
+            document.getElementById("response").src = "pics/Mac.jpg";
         } else if (loc ==1 && pet == 4){
             document.getElementById("responseText").innerHTML = ("Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/")  
+            document.getElementById("response").src = "pics/ArtieCar.jpg";
         } else if (loc == 2 && pet == 1){
             document.getElementById("responseText").innerHTML = ("Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!")  
+            document.getElementById("response").src = "pics/DeeWalk.jpg";
         } else if (loc == 2 && pet == 2){
             document.getElementById("responseText").innerHTML = ("Dennis smiles and stays by your side throughout the walk :)")  
+            document.getElementById("response").src = "pics/side-walk.jpg";
         } else if (loc == 2 && pet == 3){
             document.getElementById("responseText").innerHTML = ("Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.")  
+            document.getElementById("response").src = "pics/walk-time.jpg";
         } else if (loc == 2 && pet == 4){
             document.getElementById("responseText").innerHTML = ("Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.")  
+            document.getElementById("response").src = "pics/Artie.jpg";
         } else if (loc == 3 && pet == 1){
             document.getElementById("responseText").innerHTML = ("Dee is scared. She hears the bath water, tucks her tail ebtween her legs, and hurries to the comfort of her kennel. Dennis begins to bark in her defense.")  
+            document.getElementById("response").src = "pics/SadDee.jpg";
         } else if (loc == 3 && pet == 2){
             document.getElementById("responseText").innerHTML = ("Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)")  
+            document.getElementById("response").src = "pics/DennisBath.jpg";
         } else if (loc == 3 && pet == 3){
             document.getElementById("responseText").innerHTML = ("Mac enjoys the feel of the water and is a trooper through the process.")  
+            document.getElementById("response").src = "pics/MacBath.jpg";
         } else if (loc == 3 && pet == 4){
             document.getElementById("responseText").innerHTML = ("Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear.")  
+            document.getElementById("response").src = "pics/Artie2.jpg";
         } 
 }
