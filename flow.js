14cabdd055005ef5665b5538d849b47724f5dc60
Keith Bailey <bailey.keith.allen@gmail.com>
2020-08-11 09:00:20 -0400

    pet array layout fixed

--- flow.js
+++ flow.js
@@ -3,11 +3,13 @@ let html = " ";
 for (let i=0; i<pets.length; i++) {
     let pet = pets[i];
     html += `
-    <img src="${pet.pic}" alt="picture of ${pet.name}"
+    <div class="col" id="${pet.id}">
+    <img src="${pet.pic}" alt="picture of ${pet.name}">
     <h2>${pet.name}</h2>
-    <h3>DOB: ${pet.DOB}</h3>
-    <p>Nicknames: ${pet.nicknames}</p>
-    <p>Favorite Toy: ${pet.toy}</p>
+    <p><strong>DOB:</strong> ${pet.DOB}</p>
+    <p><strong>Nicknames:</strong> ${pet.nicknames}</p>
+    <p><strong>Favorite Toy:</strong> ${pet.toy}</p>
+    </div>
     `
 }
 
--- pets.js
+++ pets.js
@@ -3,28 +3,31 @@ const pets = [
     DOB: "June 6, 2015",
     toy: "Green Ball", 
     nicknames: "Baby Dee, Deandra, Deb",
-    pic: "pics/DeeMain.jpg"
+    pic: "pics/DeeMain.jpg",
+    id: "primary"
 },
 {   name: "Dennis",
     DOB: "June 6, 2015",
     toy: "Soccer Ball", 
     nicknames: "Big Boy, Denald, B-Bo",
-    pic: "pics/DenMain.jpg"
-
+    pic: "pics/DenMain.jpg",
+    id: "secondary"
 },
 {
     name: "Mac",
     DOB: "November 2, 2019",
     toy: "Sticks and Bones", 
     nicknames: "Cheese, Queso, Perrita, Quesita",
-    pic: "pics/MacMain.jpg"
+    pic: "pics/MacMain.jpg",
+    id: "third"
 },
 {
     name: "Artemis",
     DOB: "Unknown",
     toy: "Anything I can fit in", 
     nicknames: "Artie, Meow-meow",
-    pic: "pics/ArtieMain.jpg"   
+    pic: "pics/ArtieMain.jpg",
+    id: "fourth"   
 }
 ]
 
