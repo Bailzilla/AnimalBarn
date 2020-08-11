fb8702b5061ccc139ec8e240c1b3cfcef38875f7
Keith Bailey <bailey.keith.allen@gmail.com>
2020-08-11 06:21:45 -0400

    begin refactoring with pet array

---
+++ pets.js
@@ -0,0 +1,31 @@
+const pets = [
+{   name: "Sweet Dee",
+    DOB: "June 6, 2015",
+    toy: "Green Ball", 
+    nicknames: "Baby Dee, Deandra, Deb",
+    img: "pics/DeeMain.jpg"
+},
+{   name: "Dennis",
+    DOB: "June 6, 2015",
+    toy: "Soccer Ball", 
+    nicknames: "Big Boy, Denald, B-Bo",
+    img: "pics/DenMain.jpg"
+
+},
+{
+    name: "Mac",
+    DOB: "November 2, 2019",
+    toy: "Sticks and Bones", 
+    nicknames: "Cheese, Queso, Perrita, Quesita",
+    img: "pics/MacMain.jpg"
+},
+{
+    name: "Artemis",
+    DOB: "Unknown",
+    toy: "Anything I can fit in", 
+    nicknames: "Artie, Meow-meow",
+    img: "pics/ArtieMain.jpg"   
+}
+
+]
+
