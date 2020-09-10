08cc04724b0c6806b31e0c82807b8b659919813a
Keith Bailey <bailey.keith.allen@gmail.com>
2020-09-10 14:26:53 -0400

    created Pet class for DRY

--- js/pets.js
+++ js/pets.js
@@ -1,33 +1,18 @@
-const pets = [
-{   name: "Sweet Dee",
-    DOB: "June 6, 2015",
-    toy: "Green Ball", 
-    nicknames: "Baby Dee, Deandra, Deb",
-    pic: "pics/DeeMain.jpg",
-    id: "primary"
-},
-{   name: "Dennis",
-    DOB: "June 6, 2015",
-    toy: "Soccer Ball", 
-    nicknames: "Big Boy, Denald, B-Bo",
-    pic: "pics/DenMain.jpg",
-    id: "secondary"
-},
-{
-    name: "Mac",
-    DOB: "November 2, 2019",
-    toy: "Sticks and Bones", 
-    nicknames: "Cheese, Queso, Perrita, Quesita",
-    pic: "pics/MacMain.jpg",
-    id: "third"
-},
-{
-    name: "Artemis",
-    DOB: "Unknown",
-    toy: "Anything I can fit in", 
-    nicknames: "Artie, Meow-meow",
-    pic: "pics/ArtieMain.jpg",
-    id: "fourth"   
+class Pet {
+    constructor(name,DOB,toy,nick,pic,id){
+        this.name = name
+        this.DOB = DOB
+        this.toy = toy
+        this.nick = nick
+        this.pic = pic
+        this.id = id
+    }
 }
-]
+
+const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary")
+const Dennis = new Pet("Dennis","June 6, 2015","Soccer Ball","Big Boy, Denald, B-Bo","pics/DenMain.jpg","secondary")
+const Mac = new Pet("Mac","November 2, 2019","Sticks and Bones","Cheese, Queso, Perrita, Quesita","pics/MacMain.jpg","third")
+const Artie = new Pet("Artemis","Unknown","Anything I can fit in","Artie, Meow-meow","pics/ArtieMain.jpg","fourth"   )
+
+const pets = [Dee, Dennis, Mac, Artie]
 
