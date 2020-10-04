01d25aed704d0759e697b457dbdf7c2639c4b2e1
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-04 13:44:33 -0400

    step 1 of updating if/else overload

--- js/pets.js
+++ js/pets.js
@@ -1,15 +1,17 @@
 class Pet {
-    constructor(name,birth,toy,nick,pic,id){
+    constructor(name,birth,toy,nick,pic,id,resp,react){
         this.name = name
         this.birth = birth
         this.toy = toy
         this.nick = nick
         this.pic = pic
         this.id = id
+        this.resp = resp
+        this.react = react
     }
 }
 
-const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary")
+const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary",)
 const Dennis = new Pet("Dennis","June 6, 2015","Soccer Ball","Big Boy, Denald, B-Bo","pics/DenMain.jpg","secondary")
 const Mac = new Pet("Mac","November 2, 2019","Sticks and Bones","Cheese, Queso, Perrita, Quesita","pics/MacMain.jpg","third")
 const Artie = new Pet("Artemis","Unknown","Anything I can fit in","Artie, Meow-meow","pics/ArtieMain.jpg","fourth"   )
--- js/script.js
+++ js/script.js
@@ -6,40 +6,40 @@ function goAdv() {
     let loc = document.getElementById('activity').value;
     let pet = document.getElementById('pet').value;
         
-        if (loc == 1 && pet == 1){
-             document.getElementById("responseText").innerHTML = "Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze";
+        if (loc == 1 && pet == 'dee'){
+             document.getElementById("responseText").innerHTML = ;
              document.getElementById("response").src = "pics/DeeRide.jpg";
-        } else if (loc == 1 && pet == 2){
+        } else if (loc == 1 && pet == 'dennis'){
             document.getElementById("responseText").innerHTML =  ("As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.")
             document.getElementById("response").src = "pics/Dennis.jpg";
-        } else if (loc == 1 && pet == 3){
+        } else if (loc == 1 && pet == 'mac'){
             document.getElementById("responseText").innerHTML = ("Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.")
             document.getElementById("response").src = "pics/Mac.jpg";
-        } else if (loc == 1 && pet == 4){
+        } else if (loc == 1 && pet == 'artie'){
             document.getElementById("responseText").innerHTML = ("Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/")  
             document.getElementById("response").src = "pics/ArtieCar.jpg";
-        } else if (loc == 2 && pet == 1){
+        } else if (loc == 2 && pet == 'dee'){
             document.getElementById("responseText").innerHTML = ("Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!")  
             document.getElementById("response").src = "pics/DeeWalk.jpg";
-        } else if (loc == 2 && pet == 2){
+        } else if (loc == 2 && pet == 'dennis'){
             document.getElementById("responseText").innerHTML = ("Dennis smiles and stays by your side throughout the walk :)")  
             document.getElementById("response").src = "pics/side-walk.jpg";
-        } else if (loc == 2 && pet == 3){
+        } else if (loc == 2 && pet == 'mac'){
             document.getElementById("responseText").innerHTML = ("Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.")  
             document.getElementById("response").src = "pics/MacWalk.jpg";
-        } else if (loc == 2 && pet == 4){
+        } else if (loc == 2 && pet == 'artie'){
             document.getElementById("responseText").innerHTML = ("Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.")  
             document.getElementById("response").src = "pics/Artie.jpg";
-        } else if (loc == 3 && pet == 1){
+        } else if (loc == 3 && pet == 'dee'){
             document.getElementById("responseText").innerHTML = ("Dee is scared as she hears the bath water. She tries to run, but you are able to wrangle her; eventually she calms down and enjoys the warm water.")  
             document.getElementById("response").src = "pics/DeeBath.jpg";
-        } else if (loc == 3 && pet == 2){
+        } else if (loc == 3 && pet == 'dennis'){
             document.getElementById("responseText").innerHTML = ("Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)")  
             document.getElementById("response").src = "pics/DenBath.jpg";
-        } else if (loc == 3 && pet == 3){
+        } else if (loc == 3 && pet == 'mac'){
             document.getElementById("responseText").innerHTML = ("Mac enjoys the feel of the water and is a trooper through the process.")  
             document.getElementById("response").src = "pics/MacBath.jpg";
-        } else if (loc == 3 && pet == 4){
+        } else if (loc == 3 && pet == 'artie'){
             document.getElementById("responseText").innerHTML = ("Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear.")  
             document.getElementById("response").src = "pics/Artie2.jpg";
         } 
