30a4329f04196068a767b2b9f71707d73d33e445
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-07 00:25:44 -0400

    stage 2 of nested array construction

--- js/pets.js
+++ js/pets.js
@@ -11,9 +11,15 @@ class Pet {
     }
 }
 
-const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary",)
+const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary",[1,2,3],[4,5,6])
+console.log(Dee.react[2])
+
 const Dennis = new Pet("Dennis","June 6, 2015","Soccer Ball","Big Boy, Denald, B-Bo","pics/DenMain.jpg","secondary")
+
+
 const Mac = new Pet("Mac","November 2, 2019","Sticks and Bones","Cheese, Queso, Perrita, Quesita","pics/MacMain.jpg","third")
+
+
 const Artie = new Pet("Artemis","Unknown","Anything I can fit in","Artie, Meow-meow","pics/ArtieMain.jpg","fourth"   )
 
 const pets = [Dee, Dennis, Mac, Artie]
