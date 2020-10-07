c9a6f332865a43035cc01fcf3b6ea9303dd82b6e
Keith Bailey <bailey.keith.allen@gmail.com>
2020-10-07 00:56:06 -0400

    refactoring of if/else hell complete :) enjoy your simplified code

--- js/pets.js
+++ js/pets.js
@@ -11,16 +11,41 @@ class Pet {
     }
 }
 
-const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary",[1,2,3],[4,5,6])
+const Dee = new Pet("Sweet Dee","June 6, 2015","Green Ball", "Baby Dee, Deandra, Deb", "pics/DeeMain.jpg","primary",
+["Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze",
+"Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!",
+"Dee is scared as she hears the bath water. She tries to run, but you are able to wrangle her; eventually she calms down and enjoys the warm water."],
+["pics/DeeRide.jpg",
+"pics/DeeWalk.jpg",
+"pics/DeeBath.jpg"])
+
 console.log(Dee.react[2])
 
-const Dennis = new Pet("Dennis","June 6, 2015","Soccer Ball","Big Boy, Denald, B-Bo","pics/DenMain.jpg","secondary")
+const Dennis = new Pet("Dennis","June 6, 2015","Soccer Ball","Big Boy, Denald, B-Bo","pics/DenMain.jpg","secondary",
+["As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.",
+"Dennis smiles and stays by your side throughout the walk :)",
+"Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)"],
+["pics/Dennis.jpg",
+"pics/side-walk.jpg",
+"pics/DenBath.jpg"])
 
 
-const Mac = new Pet("Mac","November 2, 2019","Sticks and Bones","Cheese, Queso, Perrita, Quesita","pics/MacMain.jpg","third")
+const Mac = new Pet("Mac","November 2, 2019","Sticks and Bones","Cheese, Queso, Perrita, Quesita","pics/MacMain.jpg","third",
+["Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.",
+"Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.",
+"Mac enjoys the feel of the water and is a trooper through the process."],
+["pics/Mac.jpg",
+"pics/MacWalk.jpg",
+"pics/MacBath.jpg"])
 
 
-const Artie = new Pet("Artemis","Unknown","Anything I can fit in","Artie, Meow-meow","pics/ArtieMain.jpg","fourth"   )
+const Artie = new Pet("Artemis","Unknown","Anything I can fit in","Artie, Meow-meow","pics/ArtieMain.jpg","fourth",
+["Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/",
+"Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.",
+"Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear."],
+["pics/ArtieCar.jpg",
+"pics/Artie.jpg",
+"pics/Artie2.jpg"])
 
 const pets = [Dee, Dennis, Mac, Artie]
 
--- js/script.js
+++ js/script.js
@@ -2,45 +2,8 @@
 //From there it assesses the selections and responds with the chosen pet's reaction to the activity.
 
 function goAdv() {
-
     let loc = document.getElementById('activity').value;
-    let pet = document.getElementById('pet').value;
-        
-        if (loc == 1 && pet == 'Dee'){
-             document.getElementById("responseText").innerHTML = "Great choice! Sweet Dee loves going for a ride, just be sure to crack the window so she can feel the breeze";
-             document.getElementById("response").src = "pics/DeeRide.jpg";
-        } else if (loc == 1 && pet == 'Dennis'){
-            document.getElementById("responseText").innerHTML =  ("As an ambulance siren goes by, Dennis begins to howl from the depth of his chest. Your ear drums are ratteld, but you ultimately walk away okay.")
-            document.getElementById("response").src = "pics/Dennis.jpg";
-        } else if (loc == 1 && pet == 'Mac'){
-            document.getElementById("responseText").innerHTML = ("Mac is cautiously excited. She paces back and forth from window to window in the backseat, absorbing everything that comes her way.")
-            document.getElementById("response").src = "pics/Mac.jpg";
-        } else if (loc == 1 && pet == 'Artie'){
-            document.getElementById("responseText").innerHTML = ("Artie is NOT happy with you. At a red light she cralwls into your lap and goes to the bathroom :/")  
-            document.getElementById("response").src = "pics/ArtieCar.jpg";
-        } else if (loc == 2 && pet == 'Dee'){
-            document.getElementById("responseText").innerHTML = ("Dee howls uncontrollably as you strap on her lead. Away you go, hope you brought the right shoes!")  
-            document.getElementById("response").src = "pics/DeeWalk.jpg";
-        } else if (loc == 2 && pet == 'Dennis'){
-            document.getElementById("responseText").innerHTML = ("Dennis smiles and stays by your side throughout the walk :)")  
-            document.getElementById("response").src = "pics/side-walk.jpg";
-        } else if (loc == 2 && pet == 'Mac'){
-            document.getElementById("responseText").innerHTML = ("Mac begins barking as soon as she sees you with her leash. Along the walk she zig-zags across the sidewalk, smelling everything in her path.")  
-            document.getElementById("response").src = "pics/MacWalk.jpg";
-        } else if (loc == 2 && pet == 'Artie'){
-            document.getElementById("responseText").innerHTML = ("Artie may not do collars, but she is a willing follower in a walk around the block. She stalks 10 yards behind you, lurking from porch to porch.")  
-            document.getElementById("response").src = "pics/Artie.jpg";
-        } else if (loc == 3 && pet == 'Dee'){
-            document.getElementById("responseText").innerHTML = ("Dee is scared as she hears the bath water. She tries to run, but you are able to wrangle her; eventually she calms down and enjoys the warm water.")  
-            document.getElementById("response").src = "pics/DeeBath.jpg";
-        } else if (loc == 3 && pet == 'Dennis'){
-            document.getElementById("responseText").innerHTML = ("Dennis is hesitant at first, but he calms down once he feels the water. Good boy :)")  
-            document.getElementById("response").src = "pics/DenBath.jpg";
-        } else if (loc == 3 && pet == 'Mac'){
-            document.getElementById("responseText").innerHTML = ("Mac enjoys the feel of the water and is a trooper through the process.")  
-            document.getElementById("response").src = "pics/MacBath.jpg";
-        } else if (loc == 3 && pet == 'Artie'){
-            document.getElementById("responseText").innerHTML = ("Artie cleans herself, and is therefore above being bathed by a mere human. She uses her elusiveness to disapear.")  
-            document.getElementById("response").src = "pics/Artie2.jpg";
-        } 
+    let who = document.getElementById('who').value;
+    document.getElementById("responseText").innerHTML = pets[who].resp[loc]
+    document.getElementById("response").src = pets[who].react[loc] 
 }
